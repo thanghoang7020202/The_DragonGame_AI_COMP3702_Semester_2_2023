68230475291ef9919aa53d44574ada21951efd82
Hoang Cao Quoc Thang <thang.hoang7020202@hcmut.edu.vn>
2023-08-19 21:41:15 +1000

    using different container, separate gems and exit

--- a1-support/solution.py
+++ a1-support/solution.py
@@ -23,6 +23,16 @@ class ContainerEntry:
     def __lt__(self, other):
         return self.cost < other.cost
 
+class ContainerEntryAStar:
+    def __init__(self, state, path, cost, heuristic_cost):
+        self.state = state
+        self.path = path
+        self.cost = cost
+        self.heuristic_cost = heuristic_cost + cost
+    
+    def __lt__(self, other):
+        return self.heuristic_cost < other.heuristic_cost
+        
 class Solver:
 
     def __init__(self, game_env):
@@ -78,36 +88,35 @@ class Solver:
 
         pass
 
+    def gem_distance(self, state):
+        gem = state.gem_status.index(0)
+        # get position to the gem
+        gem_pos = self.game_env.gem_positions[gem]
+        if gem_pos[0] > state.row: # target (gem, goal) is below the current state
+            # compute Euclidean distance
+            return ((state.row - gem_pos[0]) ** 2 + (state.col - gem_pos[1]) ** 2) ** 0.5
+        else:
+            # compute Manhattan distance
+            return (abs(state.row - gem_pos[0]) + abs(state.col - gem_pos[1]))
+        
     def compute_heuristic(self, state):
         """
         Compute a heuristic value h(n) for the given state.
         :param state: given state (GameState object)
         :return a real number h(n)
         """
-        if 0 in state.gem_status:
-            # get the index of the first uncollected gem
-            gem = state.gem_status.index(0)
-            # get position to the gem
-            gem_pos = self.game_env.gem_positions[gem]
-            if gem_pos[0] > state.row: # target (gem, goal) is below the current state
-                # compute Euclidean distance
-                return ((state.row - gem_pos[0]) ** 2 + (state.col - gem_pos[1]) ** 2) ** 0.5
-            else:
-                # compute Manhattan distance
-                return (abs(state.row - gem_pos[0]) + abs(state.col - gem_pos[1]))
-        # if all gems are collected, compute distance to the goal
-        else: 
-            if self.game_env.exit_row > state.row:
-                return ((state.row - self.game_env.exit_row) ** 2 + (state.col - self.game_env.exit_col) ** 2) ** 0.5
-            else:
-                return (abs(state.row - self.game_env.exit_row) + abs(state.col - self.game_env.exit_col))
+        if self.game_env.exit_row > state.row:
+            return ((state.row - self.game_env.exit_row) ** 2 + (state.col - self.game_env.exit_col) ** 2) ** 0.5
+        else:
+            return (abs(state.row - self.game_env.exit_row) + abs(state.col - self.game_env.exit_col))
+        
 
     def search_a_star(self):
         """
         Find a path which solves the environment using A* Search.
         :return: path (list of actions, where each action is an element of GameEnv.ACTIONS)
         """
-        container = [ContainerEntry(self.game_env.get_init_state(), [], 0)]
+        container = [ContainerEntryAStar(self.game_env.get_init_state(), [], 0, 0)]
         heapq.heapify(container)
         visited = set()
         while container:
@@ -120,7 +129,12 @@ class Solver:
                 if success and persistent_state not in visited and not self.game_env.is_game_over(persistent_state):
                     if self.game_env.is_solved(persistent_state):
                         return Entry.path + [action]
-                    node =  ContainerEntry(persistent_state, Entry.path + [action], Entry.cost + self.game_env.ACTION_COST[action] \
-                                           + self.compute_heuristic(persistent_state) - self.compute_heuristic(Entry.state))
+                    if 0 in persistent_state.gem_status:
+                        heuristic_cost = self.gem_distance(persistent_state)
+                    else:
+                        heuristic_cost = self.compute_heuristic(persistent_state)
+                    node =  ContainerEntryAStar(persistent_state, Entry.path + [action], \
+                                                Entry.cost + self.game_env.ACTION_COST[action], \
+                                                heuristic_cost)
                     heapq.heappush(container, node)
         return []
